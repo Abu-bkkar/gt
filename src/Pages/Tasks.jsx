91e0270d57dfc93e3aeb889c5c4d2e1c4e8fe6d8
Apple <smsamimkhan54@gmail.com>
2025-12-06 21:16:35 +0600

    first commit

--- src/Pages/Tasks.jsx
+++ src/Pages/Tasks.jsx
@@ -8,7 +8,7 @@ import ShowTasks from "../components/tasks/ShowTasks";
 
 function Tasks() {
     const [tasks, setTasks] = useState([]);
-    // const [visibility, setVisibility] = useState('all');
+    const [visibility, setVisibility] = useState('all');
 
     const addNewTask = (text) => {
       const task ={
@@ -18,12 +18,38 @@ function Tasks() {
       }
         setTasks([...tasks, task]);
     }
+ const toggleCompletion = (id) => {
+    const newTaskes = tasks.map((task) => {
+      if (task.id === id) {
+        return { ...task, isCompleted: !task.isCompleted };
+      } else {
+        return task;
+      }   
+    });
+    setTasks(newTaskes);
+ };
+    const changeVisibility = (newVisibility) => {
+      setVisibility(newVisibility);
+    };
+ 
+    function getFilteredTasks() {
+      if (visibility === 'completed') {
+        return tasks.filter((task) => task.isCompleted);
+      } else if (visibility === 'incomplete') {
+        return tasks.filter((task) => !task.isCompleted);
+      } else {
+        return tasks;
+      }
+    }
 
   return (
     <Layout>
       <h1>Tasks</h1>
       <CreateTask addNewTask={addNewTask} />
-      <ShowTasks tasks={tasks} />
+      <ShowTasks tasks={getFilteredTasks()} toggleCompletion={toggleCompletion} />
+      <button onClick={() => changeVisibility('all')}>All</button>
+      <button onClick={() => changeVisibility('completed')}>Completed</button>
+      <button onClick={() => changeVisibility('incomplete')}>Incomplete</button>
     </Layout>
     
   );
--- src/components/tasks/ShowTasks.jsx
+++ src/components/tasks/ShowTasks.jsx
@@ -1,11 +1,11 @@
 
 
 
-function ShowTasks({tasks}) {  return (
+function ShowTasks({tasks, toggleCompletion}) {  return (
     <div>
      {tasks.length > 0 ? <ul>
         {tasks.map((task) => (
-            <li key={task.id}>{task.text}</li>
+            <li key={task.id} onClick={() => toggleCompletion(task.id)} style={{textDecoration: task.isCompleted ? 'line-through' : 'none'}}>{task.text}</li>
         ))}
      </ul>  : <p>No tasks available</p>}
     </div>
